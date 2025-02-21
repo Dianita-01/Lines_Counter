970190309d30ef5867862e0229d3493f967e2938
Juan-Trivellari <A21216380@alumnos.uayd.mx>
2025-02-21 15:14:19 -0600

    build: :sparkles: Programa ya lee
    para poder ejecutarlo en sus compus tienen que tener minimo el JDK 17 y en la linea de comandos escriven javac Main.java y luego java .\Main.java

---
+++ src/CodeValidatorContext.java
@@ -0,0 +1,16 @@
+
+public class CodeValidatorContext {
+    private SpecificRules validator;
+
+    public void setValidator(SpecificRules validator) {
+        this.validator = validator;
+    }
+
+    public boolean validateCode(String code) {
+        if (validator == null) {
+            System.out.println("No se ha establecido un validador.");
+            return false;
+        }
+        return validator.validate(code);
+    }
+}
---
+++ src/HandleInput.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class HandleInput {
+    public void processInput(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        if (args.length == 0) {
+            System.out.print("Introduce la ruta del archivo o directorio: ");
+            String path = scanner.nextLine();
+            processPath(path);
+        } else {
+            processPath(args[0]);
+        }
+    }
+
+    private void processPath(String path) {
+        File file = new File(path);
+
+        if (file.isDirectory()) {
+            DirectoryFileCounter directoryFileCounter = new DirectoryFileCounter();
+            directoryFileCounter.countLinesInDirectory(file);
+        } else if (file.isFile()) {
+            FileCounter fileCounter = new FileCounter();
+            fileCounter.countLinesInFile(file);
+        } else {
+            System.out.println("La ruta proporcionada no es valida.");
+        }
+    }
+}
